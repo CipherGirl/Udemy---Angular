fc377a381594ef9b273425d176290ee552b8dd56
HasnaHenaMow <hasna.itsd@goldenharvestbd.com>
2023-11-29 15:58:49 +0600

    Directives (NgStyle and NgClass)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,12 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  showSecret = true;
+  logs = [];
+
+  handleClick() {
+    this.showSecret = !this.showSecret;
+    this.logs.push(`${Date()} Button Clicked`);
+  }
+}
