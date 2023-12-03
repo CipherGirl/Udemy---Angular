0bf17ed30656a1f28b79aa9955914ca98903aa20
HasnaHenaMow <hasna.itsd@goldenharvestbd.com>
2023-12-03 15:09:27 +0600

    Add Recipe List and Recipe Description Structure

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.types';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    {
+      name: 'Biriyani',
+      description: 'Aromatic Authentic Biriyani',
+      imagePath:'https://www.licious.in/blog/wp-content/uploads/2020/12/Hyderabadi-chicken-Biryani.jpg',
+    },
+    {
+      name: 'Biriyani',
+      description: 'Aromatic Authentic Biriyani',
+      imagePath:'https://www.licious.in/blog/wp-content/uploads/2020/12/Hyderabadi-chicken-Biryani.jpg',
+    },
+  ];
 }
