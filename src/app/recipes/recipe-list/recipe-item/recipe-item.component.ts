ecb6da39f92762f90983fd1175f8612d318ef775
HasnaHenaMow <hasna.itsd@goldenharvestbd.com>
2023-12-04 16:44:31 +0600

    Pass Event 2 Level Up - Event and Property Binding

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Recipe } from '../../recipe.types';
 
 @Component({
@@ -8,4 +8,9 @@ import { Recipe } from '../../recipe.types';
 })
 export class RecipeItemComponent {
   @Input() recipe: Recipe;
+  @Output() selectedRecipeEventFromItem = new EventEmitter<void>();
+
+  handleSelectedRecipeFromItem() {
+    this.selectedRecipeEventFromItem.emit();
+  }
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.types';
 
 @Component({
@@ -7,16 +7,23 @@ import { Recipe } from '../recipe.types';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
+  @Output() selectedRecipeEventFromList = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     {
       name: 'Biriyani',
       description: 'Aromatic Authentic Biriyani',
-      imagePath:'https://www.licious.in/blog/wp-content/uploads/2020/12/Hyderabadi-chicken-Biryani.jpg',
+      imagePath:
+        'https://www.licious.in/blog/wp-content/uploads/2020/12/Hyderabadi-chicken-Biryani.jpg',
     },
     {
       name: 'Biriyani',
       description: 'Aromatic Authentic Biriyani',
-      imagePath:'https://www.licious.in/blog/wp-content/uploads/2020/12/Hyderabadi-chicken-Biryani.jpg',
+      imagePath:
+        'https://www.licious.in/blog/wp-content/uploads/2020/12/Hyderabadi-chicken-Biryani.jpg',
     },
   ];
+
+  handleSelectedRecipeFromList(recipe: Recipe) {
+    this.selectedRecipeEventFromList.emit(recipe);
+  }
 }
