f7ff2086379423e351260f3dbd406aeffa48558b
HasnaHenaMow <hasna.itsd@goldenharvestbd.com>
2023-12-12 16:06:52 +0600

    Add Ingredients in Recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.types';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
   selectedRecipe = new EventEmitter<Recipe>();
@@ -10,17 +11,19 @@ export class RecipeService {
       description: 'Aromatic Authentic Biriyani',
       imagePath:
         'https://www.licious.in/blog/wp-content/uploads/2020/12/Hyderabadi-chicken-Biryani.jpg',
+      ingredients: [new Ingredient('Meat', 1), new Ingredient('Rice', 1)],
     },
+
     {
       name: 'Lamb Shank',
       description: 'Authentic Lamb Shank',
       imagePath:
         'https://www.billyparisi.com/wp-content/uploads/2022/04/lamb-shanks-featured.jpg',
+      ingredients: [new Ingredient('Meat', 1), new Ingredient('Potatoes', 1)],
     },
   ];
 
   getRecipe() {
     return this.recipes.slice();
   }
-
 }
