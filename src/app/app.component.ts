2a45effb0118c7559d862c3ef685e5aec9cf48b6
HasnaHenaMow <hasna.itsd@goldenharvestbd.com>
2023-12-20 17:33:24 +0600

    Template Driven Forms Assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  suggestUserName() {
-    const suggestedName = 'Superuser';
+  @ViewChild('signupForm') signupForm: NgForm;
+  subscriptions = ['Basic', 'Advanced', 'Pro'];
+  selectedSubscription = 'Advanced';
+
+  setSelectedSubscription(subscription) {
+    this.selectedSubscription = subscription;
+  }
+
+  onSubmit() {
+    console.log(this.signupForm.value );
   }
 }
